c462e7c936ba37ef0d0d5966a12543fdedae98af
AgentAsh23 <111176235+AgentAsh23@users.noreply.github.com>
2022-08-14 12:00:53 +0530

    Create P2(Input Output).java
    Integer Input=23
    Float Input=23.00
    String Input=Its me ash

---
+++ P2(Input Output).java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class InputOutput
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Integer Input=");
+        int a=sc.nextInt();
+        System.out.println("Float Input=");
+        float b=sc.nextFloat();
+        System.out.println("String Input=");
+        String c=sc.nextLine();
+    }
+}
