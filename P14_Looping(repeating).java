2002a8847e668c776d44feb1084ec61889eee7a4
AgentAsh23 <111176235+AgentAsh23@users.noreply.github.com>
2022-08-22 12:53:02 +0530

    Update P14_Looping(repeating).java

--- P14_Looping(repeating).java
+++ P14_Looping(repeating).java
@@ -4,27 +4,21 @@ class HelloWorld
     public static void main(String[] args) 
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println("What is 5 + 9?");
-        int n=sc.nextInt();
-        int numbersRead = 0;
-        int num = 1;
-
-        while (num>0) 
+        int num=1;
+        while (num>0)
         {
+            System.out.println("What is 5 + 9?");
+            int n=sc.nextInt();
             if (n == 14) 
             {
                 System.out.println("You got it");
-                break;
+                num=0;
             }
             else
             {
                 System.out.println("Wrong choice");
-                System.out.println("What is 5 + 9?");
-                num++;
-                break;
-                
+                num=1;
             }
         }
-    
     }
 }
